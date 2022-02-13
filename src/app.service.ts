3e73bf694973f651cc54aad0a5c1e2517cf1e55b
Pedro Medina Márquez <medinamarquezp@gmail.com>
2022-02-13 21:24:50 +0100

    feat: produce test message implementation

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { ProducerService } from './kafka/services/producer.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly producerService: ProducerService) {}
+
+  async produceTestMessage(): Promise<void> {
+    await this.producerService.produce({
+      topic: 'test',
+      messages: [
+        {
+          value: 'Test message',
+        },
+      ],
+    });
   }
 }
