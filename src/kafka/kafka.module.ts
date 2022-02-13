b815e2a1ad521693c6f27fceb532be5d65638687
Pedro Medina Márquez <medinamarquezp@gmail.com>
2022-02-13 21:18:11 +0100

    feat: kafka consumer service

--- src/kafka/kafka.module.ts
+++ src/kafka/kafka.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ConsumerService } from './services/consumer.service';
 import { ProducerService } from './services/producer.service';
 
 @Module({
-  providers: [ProducerService],
+  providers: [ProducerService, ConsumerService],
   imports: [ConfigModule],
-  exports: [ProducerService],
+  exports: [ProducerService, ConsumerService],
 })
 export class KafkaModule {}
---
+++ src/kafka/services/consumer.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, OnApplicationShutdown } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import {
+  Consumer,
+  ConsumerRunConfig,
+  ConsumerSubscribeTopic,
+  Kafka,
+} from 'kafkajs';
+
+@Injectable()
+export class ConsumerService implements OnApplicationShutdown {
+  constructor(private readonly configService: ConfigService) {}
+
+  private readonly kafka = new Kafka({
+    brokers: [this.configService.get<string>('KAFKA_BROKER_URL')],
+  });
+
+  private readonly consumers: Consumer[] = [];
+
+  async comsume(topic: ConsumerSubscribeTopic, config: ConsumerRunConfig) {
+    const consumer = this.kafka.consumer({ groupId: 'nestjs-kafka' });
+    await consumer.connect();
+    await consumer.subscribe(topic);
+    await consumer.run(config);
+    this.consumers.push(consumer);
+  }
+
+  async onApplicationShutdown() {
+    for (const consumer of this.consumers) {
+      await consumer.disconnect();
+    }
+  }
+}
